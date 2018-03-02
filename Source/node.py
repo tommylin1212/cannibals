7ae493c0ea2551c2bc856043b6dba6fb599ca34b
Caleb <cwill.i.am.tomlin@gmail.com>
2018-03-02 12:38:43 -0600

    fixed stupid tab shit

--- Source/node.py
+++ Source/node.py
@@ -1,17 +1,16 @@
 class Node:
-	state = []
-	parent = None
-    
+    state = []
+    parent = None
 
-    def __init__(self, state, parent,action):
-    	self.state = state
-    	self.parent = parent
-		self.action = action
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
 
     def print_n(self):
-    	print("action: ",self.action,"-> state:", self.state)
+        print("action: ", self.action, "-> state:", self.state)
 
     def print_p(self):
-    	if self.parent is not None:
-        	self.parent.print_p()
-    	self.print_n()
+        if self.parent is not None:
+            self.parent.print_p()
+        self.print_n()
